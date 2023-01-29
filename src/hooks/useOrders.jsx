4e272f72c1df3475d293177de822b136d5bd45ed
heesarang <heesarang@gmail.com>
2023-01-29 18:42:47 +0900

    all

---
+++ src/hooks/useOrders.jsx
@@ -0,0 +1,10 @@
+// hooks/useOrders.js
+
+import { useContext } from "react";
+import AppStateContext from "../contexts/AppStateContext";
+
+export default function useOrders() {
+  const { orders } = useContext(AppStateContext);
+
+  return orders;
+}
